4b7b5e68c4ccd71c94d42970c37b18d9b72d460f
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2022-12-21 17:25:48 +0100

    create a table per testsuite
    When a test file have more than none testsuite, a tests table will be
    created for each testsuite.
    
    Note: If your are using Cukinia, you can use "logging suite" to create a
    testsuite.
    
    Change-Id: Iebd51a11f7cd45f1d1d04c2dd47c114a5cf94558

--- compile.sh
+++ compile.sh
@@ -49,8 +49,9 @@ integrate_all()
 generate_row()
 {
     local i="$1"
-    local testcase="$2"
-    local xml="$3"
+    local j="$2"
+    local testcase="$3"
+    local xml="$4"
     local green_color="#90EE90"
     local red_color="#F08080"
     if [ -n "$USE_ID" ] ; then
@@ -70,7 +71,7 @@ generate_row()
         # Reset color
         echo "{set:cellbgcolor!}" >> "$TEST_ADOC_FILE"
     fi
-    if $(xmlstarlet -q sel -t  -v  "//testcase[$i]/failure" "$xml")
+    if $(xmlstarlet -q sel -t  -v "////testsuites/testsuite[$i]/testcase[$j]/failure" "$xml")
     then
         echo "|FAIL{set:cellbgcolor:$red_color}" >> "$TEST_ADOC_FILE"
     else
@@ -81,44 +82,45 @@ generate_row()
 add_xml_to_adoc()
 {
     local xml="$1"
-    local testname=$(xmlstarlet sel -t -v '//testsuite/@name' "$xml")
-    local package=$(xmlstarlet sel -t -v '//testsuite/@package' "$xml")
-    local classname=$(xmlstarlet sel -t -v '(//testcase[1])/@classname' "$xml")
-    local nb_tests=$(xmlstarlet sel -t -v '(//testsuite[1])/@tests' "$xml")
-    local failures=$(xmlstarlet sel -t -v '(//testsuite[1])/@failures' "$xml")
-    local cols='"7,1"'
-    if [ -z "$testname" -o "$testname" == "default" ] ; then
-        testname=$(basename $xml)
-    fi
-    echo -n "=== Tests $testname" >> "$TEST_ADOC_FILE"
-    if [ -n "$package" -a "$package" != "default" ] ; then
-        echo -n " package $package" >> "$TEST_ADOC_FILE"
-    fi
-    if [ -n "$classname" -a "$classname" != "cukinia" ] ; then
-        echo -n " for $classname" >> "$TEST_ADOC_FILE"
-    fi
-    echo >> "$TEST_ADOC_FILE"
-    if [ -n "$USE_ID" ] ; then
-        cols='"2,7,1"'
-    fi
+    local nb_suites="$(( $(xmlstarlet sel -t -v  '//testsuite/@name' "$xml"\
+        |wc -l) +1 ))"
+    for i in $(seq 1 ${nb_suites}) ; do
+        local testname=$(xmlstarlet sel -t -v "//testsuite[$i]/@name" "$xml")
+        local classname=$(xmlstarlet sel -t -v "//testsuite[$i]/testcase[1]/@classname" "$xml")
+        local nb_tests=$(xmlstarlet sel -t -v "//testsuite[$i]/@tests" "$xml")
+        local failures=$(xmlstarlet sel -t -v "//testsuite[$i]/@failures" "$xml")
+        local cols='"7,1"'
+        if [ -z "$testname" -o "$testname" == "default" ] ; then
+            testname=$(basename $xml)
+        fi
+        echo -n "=== Tests $testname" >> "$TEST_ADOC_FILE"
+        if [ -n "$classname" -a "$classname" != "cukinia" ] ; then
+            echo -n " for $classname" >> "$TEST_ADOC_FILE"
+        fi
+        echo >> "$TEST_ADOC_FILE"
+        if [ -n "$USE_ID" ] ; then
+            cols='"2,7,1"'
+        fi
 
-    echo "[options=\"header\",cols=$cols,frame=all, grid=all]" >> "$TEST_ADOC_FILE"
-    echo "|===" >> "$TEST_ADOC_FILE"
-    if [ -n "$USE_ID" ] ; then
-        echo -n "|Test ID" >> "$TEST_ADOC_FILE"
-    fi
-    echo "|Tests |Results" >> "$TEST_ADOC_FILE"
-    local i=1
-    while read testcase ; do
-        generate_row "$i" "$testcase" "$xml"
-        let i++
-    done < <(xmlstarlet sel -t -v '//testcase/@name' "$xml")
-    echo "|===" >> "$TEST_ADOC_FILE"
-    echo "{set:cellbgcolor!}" >> "$TEST_ADOC_FILE"
-    echo "" >> "$TEST_ADOC_FILE"
-    echo "* number of tests: $nb_tests" >> "$TEST_ADOC_FILE"
-    echo "* number of failures: $failures" >> "$TEST_ADOC_FILE"
-    echo "" >> "$TEST_ADOC_FILE"
+        echo "[options=\"header\",cols=$cols,frame=all, grid=all]" >> "$TEST_ADOC_FILE"
+        echo "|===" >> "$TEST_ADOC_FILE"
+        if [ -n "$USE_ID" ] ; then
+            echo -n "|Test ID" >> "$TEST_ADOC_FILE"
+        fi
+        echo "|Tests |Results" >> "$TEST_ADOC_FILE"
+
+        local j=1
+        while read testcase ; do
+            generate_row "$i" "$j" "$testcase" "$xml"
+            let j++
+        done < <(xmlstarlet sel -t -v "//testsuite[$i]/testcase/@name" "$xml")
+        echo "|===" >> "$TEST_ADOC_FILE"
+        echo "{set:cellbgcolor!}" >> "$TEST_ADOC_FILE"
+        echo "" >> "$TEST_ADOC_FILE"
+        echo "* number of tests: $nb_tests" >> "$TEST_ADOC_FILE"
+        echo "* number of failures: $failures" >> "$TEST_ADOC_FILE"
+        echo "" >> "$TEST_ADOC_FILE"
+    done
 
 }
 
