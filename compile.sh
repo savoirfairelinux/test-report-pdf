0a35984b9ebd4b4d559b3adbfe1658d8dbcd6529
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-04-27 15:53:23 +0200

    compile.sh: refactor
    Refactor and rename variable in preparation for future work.
    
    SFL: #8700
    Change-Id: Iffd31d0695f00b5afc81d5b026d6d3d5aa4f487f

--- compile.sh
+++ compile.sh
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-TEST_ADOC_FILE=$(readlink -f "test-reports-content.adoc")
+TMP_ADOC_FILE=$(readlink -f "test-reports-content.adoc")
 
 # Standard help message
 usage()
@@ -53,25 +53,25 @@ die()
     exit 1
 }
 
-integrate_all()
+generate_adoc()
 {
     [ -d "$SRC_DIR" ] || die "$SRC_DIR does not exist"
-    [ -f "$TEST_ADOC_FILE" ] && rm "$TEST_ADOC_FILE"
+    [ -f "$TMP_ADOC_FILE" ] && rm "$TMP_ADOC_FILE"
 
-    echo "include::$SRC_DIR/prerequisites.adoc[opts=optional]" > "$TEST_ADOC_FILE"
-    echo "" >> "$TEST_ADOC_FILE" # new line needed for a new page in asciidoc
+    echo "include::$SRC_DIR/prerequisites.adoc[opts=optional]" > "$TMP_ADOC_FILE"
+    echo "" >> "$TMP_ADOC_FILE" # new line needed for a new page in asciidoc
 
-    echo "== Test reports" >> "$TEST_ADOC_FILE"
+    echo "== Test reports" >> "$TMP_ADOC_FILE"
 
     for f in $(find "$SRC_DIR" -name "*.xml"); do
-        echo "including $f into $TEST_ADOC_FILE"
-        add_xml_to_adoc "$f" "$TEST_ADOC_FILE"
+        echo "including test file $f"
+        add_test_file_to_adoc "$f"
     done
 
-    echo "include::$SRC_DIR/notes.adoc[opts=optional]" >> "$TEST_ADOC_FILE"
+    echo "include::$SRC_DIR/notes.adoc[opts=optional]" >> "$TMP_ADOC_FILE"
 }
 
-generate_row()
+generate_test_row()
 {
     local i="$1"
     local j="$2"
@@ -90,25 +90,25 @@ generate_row()
             testcase_name="$test_id"
             test_id=""
         fi
-        echo "|${test_id}" >> "$TEST_ADOC_FILE"
+        echo "|${test_id}" >> "$TMP_ADOC_FILE"
         # Reset color
-        echo "{set:cellbgcolor!}" >> "$TEST_ADOC_FILE"
-        echo "|${testcase_name}" >> "$TEST_ADOC_FILE"
+        echo "{set:cellbgcolor!}" >> "$TMP_ADOC_FILE"
+        echo "|${testcase_name}" >> "$TMP_ADOC_FILE"
     else
         testcase_name="$testcase"
-        echo "|${testcase_name}" >> "$TEST_ADOC_FILE"
+        echo "|${testcase_name}" >> "$TMP_ADOC_FILE"
         # Reset color
-        echo "{set:cellbgcolor!}" >> "$TEST_ADOC_FILE"
+        echo "{set:cellbgcolor!}" >> "$TMP_ADOC_FILE"
     fi
     if $(xmlstarlet -q sel -t  -v "////testsuites/testsuite[$i]/testcase[$j]/failure" "$xml")
     then
-        echo "|FAIL{set:cellbgcolor:$red_color}" >> "$TEST_ADOC_FILE"
+        echo "|FAIL{set:cellbgcolor:$red_color}" >> "$TMP_ADOC_FILE"
     else
-        echo "|PASS{set:cellbgcolor:$green_color}" >> "$TEST_ADOC_FILE"
+        echo "|PASS{set:cellbgcolor:$green_color}" >> "$TMP_ADOC_FILE"
     fi
 }
 
-add_xml_to_adoc()
+add_test_file_to_adoc()
 {
     local xml="$1"
     local nb_suites="$(( $(xmlstarlet sel -t -v  '//testsuite/@name' "$xml"\
@@ -122,34 +122,34 @@ add_xml_to_adoc()
         if [ -z "$testname" -o "$testname" == "default" ] ; then
             testname=$(basename $xml)
         fi
-        echo -n "=== Tests $testname" >> "$TEST_ADOC_FILE"
+        echo -n "=== Tests $testname" >> "$TMP_ADOC_FILE"
         if [ -n "$classname" -a "$classname" != "cukinia" ] ; then
-            echo -n " for $classname" >> "$TEST_ADOC_FILE"
+            echo -n " for $classname" >> "$TMP_ADOC_FILE"
         fi
-        echo >> "$TEST_ADOC_FILE"
+        echo >> "$TMP_ADOC_FILE"
         if [ -n "$USE_ID" ] ; then
             cols='"2,7,1"'
         fi
 
-        echo "[options=\"header\",cols=$cols,frame=all, grid=all]" >> "$TEST_ADOC_FILE"
-        echo "|===" >> "$TEST_ADOC_FILE"
+        echo "[options=\"header\",cols=$cols,frame=all, grid=all]" >> "$TMP_ADOC_FILE"
+        echo "|===" >> "$TMP_ADOC_FILE"
         if [ -n "$USE_ID" ] ; then
-            echo -n "|Test ID" >> "$TEST_ADOC_FILE"
+            echo -n "|Test ID" >> "$TMP_ADOC_FILE"
         fi
-        echo "|Tests |Results" >> "$TEST_ADOC_FILE"
+        echo "|Tests |Results" >> "$TMP_ADOC_FILE"
 
         local j=1
         local testcases=$(xmlstarlet sel -t -v "//testsuite[$i]/testcase/@name" "$xml")
         echo "$testcases" | while read testcase ; do
-            generate_row "$i" "$j" "$testcase" "$xml"
+            generate_test_row "$i" "$j" "$testcase" "$xml"
             let j++
         done
-        echo "|===" >> "$TEST_ADOC_FILE"
-        echo "{set:cellbgcolor!}" >> "$TEST_ADOC_FILE"
-        echo "" >> "$TEST_ADOC_FILE"
-        echo "* number of tests: $nb_tests" >> "$TEST_ADOC_FILE"
-        echo "* number of failures: $failures" >> "$TEST_ADOC_FILE"
-        echo "" >> "$TEST_ADOC_FILE"
+        echo "|===" >> "$TMP_ADOC_FILE"
+        echo "{set:cellbgcolor!}" >> "$TMP_ADOC_FILE"
+        echo "" >> "$TMP_ADOC_FILE"
+        echo "* number of tests: $nb_tests" >> "$TMP_ADOC_FILE"
+        echo "* number of failures: $failures" >> "$TMP_ADOC_FILE"
+        echo "" >> "$TMP_ADOC_FILE"
     done
 }
 
@@ -178,7 +178,7 @@ if [ -z "$SRC_DIR" ] || [ ! -d "$SRC_DIR" ] ; then
     die "$SRC_DIR is not found or is not a directory"
 fi
 
-integrate_all
+generate_adoc
 asciidoctor-pdf test-report.adoc
 
-rm $TEST_ADOC_FILE
+rm $TMP_ADOC_FILE
