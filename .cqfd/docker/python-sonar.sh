582a5c2897855499b6895d54abd43135661fc2d4
Aiswarya Kaitheri Kandoth <aiswarya.kandoth@savoirfairelinux.com>
2024-10-29 11:49:34 +0100

    sonarqube: update sonarqube scripts
    This updates sonarqube scripts to make it work with Jenkins.

--- .cqfd/docker/python-sonar.sh
+++ .cqfd/docker/python-sonar.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 pylint --exit-zero $(fdfind -e py --exclude conf.py) \
+    --output-format=parseable \
     >pylint-report.txt
 /opt/sonar-scanner/bin/sonar-scanner
 
-detect_sonar_errors.py
+if [ "$1" != "--pylint-only" ]; then
+    /opt/sonar-scanner/bin/sonar-scanner
+fi
