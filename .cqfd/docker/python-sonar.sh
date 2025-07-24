421796c849031be786555b88e2d3de88c849af46
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2025-07-24 16:04:15 +0200

    cqfd: fix launching of SonarQube scanner in Docker
    With the update of SonarQube server, the project was no more able to
    launch sonarqube scanner in Docker container.
    There is still an issue getting the result:
    No pull request found for SonarQube task
    
    SFL: #17866
    Change-Id: I0b20b75a561dca8efeb6f64dcb0c8986ecfdf499

--- .cqfd/docker/python-sonar.sh
+++ .cqfd/docker/python-sonar.sh
@@ -3,8 +3,13 @@ set -e
 pylint --exit-zero $(fdfind -e py --exclude conf.py) \
     --output-format=parseable \
     >pylint-report.txt
-/opt/sonar-scanner/bin/sonar-scanner
+export SONAR_SCANNER_OPTS="--add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
 
 if [ "$1" != "--pylint-only" ]; then
-    /opt/sonar-scanner/bin/sonar-scanner
+    if [ -n "$GERRIT_CHANGE_NUMBER" ]; then
+        GERRIT_ARGS="-Dsonar.pullrequest.key=${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER} \
+            -Dsonar.pullrequest.base=${GERRIT_BRANCH} -Dsonar.pullrequest.branch=${GERRIT_REFSPEC} \
+            -Dsonar.scm.provider=git"
+    fi
+    /opt/sonar-scanner/bin/sonar-scanner $GERRIT_ARGS -Dsonar.token="$SONAR_AUTH_TOKEN"
 fi
